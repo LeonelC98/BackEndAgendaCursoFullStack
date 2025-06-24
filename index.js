4ddc03ff6981ae10769d5a91421f713becac6dce
LeonelC98 <Leonel18costamagna@gmail.com>
2025-06-24 14:11:11 -0300

    BackEnd agenda curso full stack

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const ramdonId =()=> Math.floor(Math.random()*1000)
+
+app.use(express.json())
+
+morgan.token('body', (req) => {
+  const body = {name: req.body.name,
+    number:req.body.number}
+  return JSON.stringify(body)
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get("/api/persons",(req,res)=> {
+  res.json(persons);
+})
+
+app.get("/info",(req,res)=>{
+  const numofcontact = persons.length
+  const fechaReq = new Date()
+
+  res.send(`<p>Phonebook has info for ${numofcontact} people</p>
+    <p>${fechaReq} </p>`)
+})
+
+app.get("/api/persons/:id",(req,res)=>{
+  const id= Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person){
+    res.json(person)
+  }else{
+    res.status(404).end()
+  }
+})
+app.post("/api/persons",(req,res)=>{
+  const person = req.body
+  const readyExist = persons.find(person1 => person1.name === person.name)
+
+  person.id = ramdonId()
+  if(!person.name){
+    return res.status(400).json({error:'Missing name'})
+  }else if (!person.number){
+    return res.status(400).json({error:'Missing number'})
+  }else if (readyExist){
+    return res.status(400).json({error:'Person already exist on Phonebook'})
+  }else{
+      persons = persons.concat(person)
+      return res.json(person)
+  }
+
+})
+
+app.delete("/api/persons/:id",(req,res)=>{
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+}
+);
